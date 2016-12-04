7e32202f41157c790484466fc53bde9ba3acede0
Dean Brundage <github@deanandadie.net>
2016-12-04 02:26:19 -0600

    stashing logging

--- lib/thermostat/state_machine_logger.rb
+++ lib/thermostat/state_machine_logger.rb
@@ -6,11 +6,15 @@ class Thermostat
     def self.extended(mod)
 
       mod.before_transition do |thing, transition|
-        thing.logger.debug(thing.class.name) { "beginning transition to #{transition.to_state}" }
+        if( thing.respond_to? :logger )
+          thing.logger.debug(thing.class.name) { "beginning transition to #{transition.to_state}" }
+        end
       end
 
       mod.after_transition do |thing, transition|
-        thing.logger.debug(thing.class.name) { "finished transition to #{transition.to_state}" }
+        if( thing.respond_to? :logger )
+          thing.logger.debug(thing.class.name) { "finished transition to #{transition.to_state}" }
+        end
       end
 
     end
